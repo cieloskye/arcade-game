e49fa61f580f2e5a1089f86e658a4cd1483eb85e
sld <hello@skyedemoya.com>
2018-12-04 19:26:53 -0800

    Created Player class, var,  & allEnemies var.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,47 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of our instances go here
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
+// Updates the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
 };
 
-// Draw the enemy on the screen, required method for game
+// Draws the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+//Player Class
+var Player = function(){
+
+    this.sprite = 'images/char-boy.png';
+
+  }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.render = function(){
+
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+  };
+
+Player.prototype.update = function(dt) {
+
+};
+
+//Player.prototype.handleInput = function(){
+
+  //}
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//Listens for key presses and sends the keys to the Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +52,11 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+
+// Objects
+
+
+const player = new Player();
+const allEnemies = [];
